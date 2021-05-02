1109790d5e17442248729971c03536766b3d5529
Catink123 <qwerty.qaz120@gmail.com>
2021-05-02 20:06:13 +0300

    dark theme tiles

--- src/themes.js
+++ src/themes.js
@@ -6,7 +6,24 @@ export default {
   night: {
     name: 'Ночная',
     gridColors: {
-      tileBorder: "white"
+      tileBorder: "white",
+      tiles: {
+        A: {bg: "#957919"},
+        F: {bg: "#9c812a"},
+        I: {bg: "#1d1c35"},
+        L: {bg: "#a69679"},
+        P: {bg: "#806406"},
+        R: {bg: "#302d29"},
+        U: {bg: "#171614"},
+        V: {bg: "#ae462e"},
+        g: {bg: "#a68e3e"},
+        m: {bg: "#ae6641"},
+        p: {bg: "#a89354"},
+        q: {bg: "#2d2d57"},
+        qZar: {bg: "#b42e10"},
+        t: {bg: "#ac8059"},
+        v: {bg: "#a8a29b"}
+      }
     }
   }
 }
